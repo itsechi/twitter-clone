0a04db9b7f6812902c80d1ca63df424c4aeec49f
itsechi <wardulinskao@gmail.com>
2023-04-04 00:54:19 +0200

    Add the feed state to the nav

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,30 @@
 import styles from './Home.module.scss';
+import React from 'react';
 
 export const Home = () => {
+  const [feed, setFeed] = React.useState('for you');
+
   return (
     <main className={styles.home}>
       <h2 className={styles.title}>Home</h2>
       <nav className={styles.nav}>
-        <a className={styles.navLink} href="#">
+        <a
+          className={styles.navLink}
+          href="#"
+          onClick={() => setFeed('for you')}
+        >
           <span className={styles.navText}>
-            For you<div className={styles.active}></div>
+            For you<div className={feed === 'for you' && styles.active}></div>
           </span>
         </a>
-        <a href="#">
-          {' '}
-          <span>
-            Following<div></div>
+        <a
+          className={styles.navLink}
+          href="#"
+          onClick={() => setFeed('following')}
+        >
+          <span className={styles.navText}>
+            Following
+            <div className={feed === 'following' && styles.active}></div>
           </span>
         </a>
       </nav>
