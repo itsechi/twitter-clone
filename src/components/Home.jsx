f676351d5e8276e10402e06ddb9c7a58c3bd8214
itsechi <wardulinskao@gmail.com>
2023-04-04 00:45:08 +0200

    Add basic styling to the home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,22 @@
+import styles from './Home.module.scss';
+
 export const Home = () => {
-  return <p>Twitter</p>;
+  return (
+    <main className={styles.home}>
+      <h2 className={styles.title}>Home</h2>
+      <nav className={styles.nav}>
+        <a className={styles.navLink} href="#">
+          <span className={styles.navText}>
+            For you<div className={styles.active}></div>
+          </span>
+        </a>
+        <a href="#">
+          {' '}
+          <span>
+            Following<div></div>
+          </span>
+        </a>
+      </nav>
+    </main>
+  );
 };
