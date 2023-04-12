b8a3542ddd73417c0c957995c5053082a5b266b0
itsechi <wardulinskao@gmail.com>
2023-04-13 00:57:00 +0200

    Add profile link to the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,16 +4,27 @@ import icons from '../../assets/icons.svg';
 export const Header = (props) => {
   return (
     <header className={styles.header}>
-      <div className={styles.headerLink}>
-        <svg>
-          <use href={`${icons}#home`}></use>
-        </svg>
-        <span className={styles.headerText}>Home</span>
-      </div>
+      <div>
+        <div className={styles.headerLink}>
+          <svg>
+            <use href={`${icons}#home`}></use>
+          </svg>
+          <span className={styles.headerText}>Home</span>
+        </div>
 
-      {/* for testing only */}
-      <div className={styles.headerLink} onClick={props.signOut}>
-        <span className={styles.headerText}>Log out</span>
+        <div className={styles.headerLink}>
+          <svg>
+            <use href={`${icons}#profile`}></use>
+          </svg>
+          <span className={styles.headerText}>Profile</span>
+        </div>
+
+        <div className={styles.headerLink} onClick={props.signOut}>
+          <svg>
+            <use href={`${icons}#more`}></use>
+          </svg>
+          <span className={styles.headerText}>Log out</span>
+        </div>
       </div>
     </header>
   );
