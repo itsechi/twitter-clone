38b5066948e8ff8c523ad5698e1041bfea6c245b
itsechi <wardulinskao@gmail.com>
2023-04-13 20:29:44 +0200

    Add profile picture to the Profile component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -16,11 +16,14 @@ export const Profile = (props) => {
         </div>
 
         <div className={styles.headerText}>
-          <h2 className={styles.headerTextLarge}>{props.user.displayName}</h2>
+          <h2 className={styles.headerTextLarge}>{user.displayName}</h2>
           <p>0 Tweets</p>
         </div>
       </div>
-      <div className={styles.images}></div>
+      <div className={styles.images}>
+        <img className={styles.banner} src="https://pbs.twimg.com/profile_banners/1557764542821765121/1680197453/600x200"></img>
+        <img className={styles.profilePic} src="https://pbs.twimg.com/profile_images/1368996766364831749/yJLY51UF_400x400.jpg"></img>
+      </div>
       <div className={styles.info}></div>
     </main>
   );
