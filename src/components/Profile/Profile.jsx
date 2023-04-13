390d1d038ff86f7a21d3a97b87f20197905ce0ae
itsechi <wardulinskao@gmail.com>
2023-04-13 22:48:04 +0200

    Add follow button to profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -15,20 +15,34 @@ export const Profile = (props) => {
             <use href={`${icons}#back`}></use>
           </svg>
         </div>
-
         <div className={styles.headerText}>
           <h2 className={styles.headerTextLarge}>{user.displayName}</h2>
           <p>0 Tweets</p>
         </div>
       </div>
+
       <div className={styles.images}>
-        <img className={styles.banner} src="https://pbs.twimg.com/profile_banners/1557764542821765121/1680197453/600x200"></img>
-        <img className={styles.profilePic} src="https://pbs.twimg.com/profile_images/1368996766364831749/yJLY51UF_400x400.jpg"></img>
+        <img
+          className={styles.banner}
+          src="https://pbs.twimg.com/profile_banners/1557764542821765121/1680197453/600x200"
+        ></img>
+        <img
+          className={styles.profilePic}
+          src="https://pbs.twimg.com/profile_images/1368996766364831749/yJLY51UF_400x400.jpg"
+        ></img>
       </div>
+
       <div className={styles.info}>
+        <div className={styles.followBar}>
+          <button className={styles.followBtn}>Follow</button>
+        </div>
+
         <h2 className={styles.displayName}>{user.displayName}</h2>
         <p className={styles.username}>@{username}</p>
-        <p className={styles.description}>User description here. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, adipisci.</p>
+        <p className={styles.description}>
+          User description here. Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Voluptatibus, adipisci.
+        </p>
       </div>
     </main>
   );
