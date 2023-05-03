1ccb476385335178a6a8e2b8ef234c4fcd6813dd
itsechi <wardulinskao@gmail.com>
2023-05-03 21:36:39 +0200

    Display logged account information in the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,70 +10,84 @@ export const Header = (props) => {
   return (
     <header className={styles.header}>
       <div className={styles.headerFixed}>
-        <Link
-          to="/home"
-          className={styles.logoLink}
-          onClick={() => setActive('home')}
-        >
-          <svg>
-            <use href={`${icons}#twitter`}></use>
-          </svg>
-        </Link>
-        <Link
-          to="/home"
-          className={styles.headerLink}
-          onClick={() => setActive('home')}
-        >
-          <svg>
-            <use
-              href={
-                active === 'home' ? `${icons}#homeClicked` : `${icons}#home`
-              }
-            ></use>
-          </svg>
-          <span
-            className={[
-              styles.headerText,
-              active === 'home' ? 'textBold' : '',
-            ].join(' ')}
+        <div>
+          <Link
+            to="/home"
+            className={styles.logoLink}
+            onClick={() => setActive('home')}
           >
-            Home
-          </span>
-        </Link>
-
-        {loggedUser && (
-          <>
-            <Link
-              to={`/${loggedUser.username}`}
-              className={styles.headerLink}
-              onClick={() => setActive('profile')}
+            <svg>
+              <use href={`${icons}#twitter`}></use>
+            </svg>
+          </Link>
+          <Link
+            to="/home"
+            className={styles.headerLink}
+            onClick={() => setActive('home')}
+          >
+            <svg>
+              <use
+                href={
+                  active === 'home' ? `${icons}#homeClicked` : `${icons}#home`
+                }
+              ></use>
+            </svg>
+            <span
+              className={[
+                styles.headerText,
+                active === 'home' ? 'textBold' : '',
+              ].join(' ')}
             >
-              <svg>
-                <use
-                  href={
-                    active === 'profile'
-                      ? `${icons}#profileClicked`
-                      : `${icons}#profile`
-                  }
-                ></use>
-              </svg>
-              <span
-                className={[
-                  styles.headerText,
-                  active === 'profile' ? 'textBold' : '',
-                ].join(' ')}
-              >
-                Profile
-              </span>
-            </Link>
+              Home
+            </span>
+          </Link>
 
-            <div className={styles.headerLink} onClick={props.signOut}>
-              <svg>
-                <use href={`${icons}#more`}></use>
-              </svg>
-              <span className={styles.headerText}>Log out</span>
+          {loggedUser && (
+            <>
+              <Link
+                to={`/${loggedUser.username}`}
+                className={styles.headerLink}
+                onClick={() => setActive('profile')}
+              >
+                <svg>
+                  <use
+                    href={
+                      active === 'profile'
+                        ? `${icons}#profileClicked`
+                        : `${icons}#profile`
+                    }
+                  ></use>
+                </svg>
+                <span
+                  className={[
+                    styles.headerText,
+                    active === 'profile' ? 'textBold' : '',
+                  ].join(' ')}
+                >
+                  Profile
+                </span>
+              </Link>
+
+              <div className={styles.headerLink} onClick={props.signOut}>
+                <svg>
+                  <use href={`${icons}#more`}></use>
+                </svg>
+                <span className={styles.headerText}>Log out</span>
+              </div>
+            </>
+          )}
+        </div>
+        {loggedUser && (
+          <Link
+            to={`/${loggedUser.username}`}
+            className={[styles.headerLink, styles.account].join(' ')}
+          >
+            <img className={[styles.profilePic, "profilePic"].join(' ')} src={loggedUser.profilePicture}></img>
+            <div className={styles.accountInfo}>
+              <p className="textBold">{loggedUser.displayName}</p>
+              <p className="textGray">@{loggedUser.username}</p>
             </div>
-          </>
+          </Link>
         )}
       </div>
     </header>
